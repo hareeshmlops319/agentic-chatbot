5b1ea083b855eec39d4a5f8817e0a343ffc35df4
hareeshmlops319 <hareesh.yalamasetty1@gmail.com>
2026-04-22 09:24:33 -0400

    adding the folder structure

---
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -0,0 +1,10 @@
+from configparser import ConfigParser
+
+class Config:
+    def __init__(self, config_file="./src/langgraphagenticai/ui/uiconfigfile.ini"):
+        self.config=ConfigParser()
+        self.config.read(config_file)
+        
+    def
+
+        
